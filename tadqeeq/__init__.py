8d7fd5154bae12e80937b332f93500abe62408c4
Mohamed Behery <mohamed.i.behery@proton.me>
2025-05-11 17:21:58 +0300

    v1.4.6

--- tadqeeq/__init__.py
+++ tadqeeq/__init__.py
@@ -5,4 +5,8 @@ Developed by Mohamed Behery @ RTR Software Development (2025-04-27).
 Licensed under the MIT License.
 """
 
-from .widgets import ImageAnnotator
+from . import widgets
+from . import utils
+from . import implementations
+
+__all__ = ['widgets', 'utils', 'implementations']
