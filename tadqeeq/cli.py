9c7ba9eea754647f5f9b8c189905cfa9246c04c5
Mohamed Behery <mohamed.i.behery@proton.me>
2025-05-27 16:08:11 +0300

    v1.11.3

--- tadqeeq/cli.py
+++ tadqeeq/cli.py
@@ -15,11 +15,7 @@ def main():
         
     parser = ArgumentParser(
         description='Tadqeeq Image Annotation Tool',
-        usage="tadqeeq [--void_background] [--verbose] [--autosave] [--use_bounding_boxes]\n\
-            --images <images_directory_path>\n--classes <class_names_filepath>\n\
-            [--bounding-boxes <bounding_boxes_directory_path>] \
-            [--semantic-segments <semantic_segments_directory_path>]\n\
-            Note:\n\tAt least one of --bounding-boxes or --semantic-segments must be provided.",
+        usage="tadqeeq [--void_background] [--verbose] [--autosave] [--use_bounding_boxes]\n--images <images_directory_path>\n--classes <class_names_filepath>\n[--bounding-boxes <bounding_boxes_directory_path>] [--semantic-segments <semantic_segments_directory_path>]\nNote:\n\tAt least one of --bounding-boxes or --semantic-segments must be provided.",
         formatter_class=RawTextHelpFormatter
     )
     
